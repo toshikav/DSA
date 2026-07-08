333a85cd00f3c887b311fa6dfc8a441ad531cf01
Toshika verma <toshikaverma9906@gmail.com>
2026-07-08 17:26:38 +0530

    Time: 0 ms (100%), Space: 42.4 MB (16.37%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.java
@@ -0,0 +1,23 @@
+class Solution {
+    long MOD = 1000000007;
+    public int countGoodNumbers(long n) {
+        long evenPositions = (n+1) / 2;
+        long oddPositions = n/2;
+
+        return (int)((expo(5, evenPositions) * expo(4, oddPositions)) % MOD);
+    }
+
+    long expo(long base, long power){
+        long ans = 1;
+
+        while (power > 0){
+            if (power % 2 == 1){
+                ans = (ans * base) % MOD;
+            }
+
+            base = (base * base) % MOD;
+            power /=2;
+        }
+        return ans;
+    }
+}
