9c8ee427f4c1f4f5e6e494852425fe67d896b498
Toshika verma <toshikaverma9906@gmail.com>
2026-07-09 02:55:18 +0530

    Time: 1 ms (99.94%), Space: 47.7 MB (91.6%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int ans = nums.length - 1;
+
+        for (int i=nums.length-2; i>=0; i--){
+            if (i + nums[i] >= ans){
+                ans = i;
+            }
+        }
+        return ans == 0;
+    }   
+}
