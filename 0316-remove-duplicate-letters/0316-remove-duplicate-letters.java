b66b94705b7b214602d81323b90c5dbed00f2c39
Toshika verma <toshikaverma9906@gmail.com>
2026-07-20 04:37:00 +0530

    Time: 2 ms (86.68%), Space: 44 MB (35.24%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        boolean visited[] = new boolean[26];
+        int freq[] = new int[26];
+
+        Stack<Character> stk = new Stack<>();
+
+        for (char ch : s.toCharArray()){
+            freq[ch - 'a']++;
+        }
+
+        for (char ch : s.toCharArray()){
+            freq[ch - 'a'] --;
+
+            if (visited[ch - 'a']){
+                continue;
+            }
+
+            while (!stk.isEmpty() && stk.peek() > ch && freq[stk.peek() - 'a'] > 0){
+                visited[stk.pop() - 'a'] = false;
+            }
+
+            stk.push(ch);
+            visited[ch - 'a'] = true;
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        for (char c : stk){
+            ans.append(c);
+        }
+        return ans.toString();
+    }
+}
