a27fcb7cf9e867acb61f97b483fe510c8b7daf2b
Toshika verma <toshikaverma9906@gmail.com>
2026-07-21 05:51:06 +0530

    Time: 7 ms (42.89%), Space: 47.1 MB (79.59%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -3,36 +3,30 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
-        while (k > 0){
-            int last = grid[m-1][n-1];
-            for (int i=m-1; i>=0; i--){
-            for (int j=n-1; j>=0; j--){
-                if (i==0 && j==0){
-                    continue;
-                
-                }
-                if (j == 0){
-                    grid[i][j] = grid[i - 1][n - 1];
-
-                }else{
-                    grid[i][j] = grid[i][j-1];
-                    }
-                }
-            }
-            grid[0][0] = last;
-            k--;
-        }
-
+        int total = m * n;
+        k %= total;
         List<List<Integer>> ans = new ArrayList<>();
 
         for (int i=0; i<m; i++){
             List<Integer> row = new ArrayList<>();
-
             for (int j=0; j<n; j++){
-                row.add(grid[i][j]);
+                row.add(0);        
             }
             ans.add(row);
-        }    
+        }   
+
+        for (int i=0; i<m; i++){
+            for (int j=0; j<n; j++){
+
+                int oldI = i * n + j;
+                int newI = (oldI + k) % total;
+
+                int newR = newI / n;
+                int newC = newI % n;
+
+                ans.get(newR).set(newC, grid[i][j]);
+            }
+        }
         return ans;
     }
 }
