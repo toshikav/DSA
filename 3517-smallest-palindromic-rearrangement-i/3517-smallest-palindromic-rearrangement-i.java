2e66ee1cbfcc02aa31899902b1e9ec690b8abade
Toshika verma <toshikaverma9906@gmail.com>
2026-07-28 22:35:37 +0530

    Time: 33 ms (43.78%), Space: 48.3 MB (38.15%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -2,21 +2,11 @@ class Solution {
     public String smallestPalindrome(String s) {
         int n = s.length();
         
-        if (n == 1){
-            return s;
-        }
-
         char half[] = s.substring(0, n/2).toCharArray();
         Arrays.sort(half);
-        String firstHalf = new String(half);
-        String middle = "";
 
-        if (n % 2 == 1){
-            middle = String.valueOf(s.charAt(n/2));
-        }
+        String ans = new String(half) + ((n % 2) == 1 ? s.charAt(n/2) : "") + new StringBuilder(new String(half)).reverse();
 
-        String secondHalf = new StringBuilder(firstHalf).reverse().toString();
-
-        return firstHalf + middle + secondHalf;
+        return ans;
     }
 }
