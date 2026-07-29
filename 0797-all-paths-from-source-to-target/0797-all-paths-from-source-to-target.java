57877a64af86caeff50a18626cb844c3e64f6f6b
Toshika verma <toshikaverma9906@gmail.com>
2026-07-29 22:59:22 +0530

    Time: 2 ms (90.74%), Space: 47.2 MB (99.53%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -1,22 +1,21 @@
 class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        List<List<Integer>> ans = new ArrayList<>();
         List<Integer> path = new ArrayList<>();
-        helper(0, graph, ans, path);
+        helper(0, graph, path);
         return ans;
 
     }
 
-    private void helper(int node, int[][] graph, List<List<Integer>> ans, List<Integer> path){
+    private void helper(int node, int[][] graph, List<Integer> path){
         path.add(node);
 
         if (node == graph.length - 1){
             ans.add(new ArrayList<>(path));
 
-        }
-        else{
+        }else{
             for (int prev : graph[node]){
-                helper(prev, graph, ans, path);
+                helper(prev, graph, path);
             }
         }
 
