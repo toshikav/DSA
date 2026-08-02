212db803afe633644eb4b166c0991e7abeffbba6
Toshika verma <toshikaverma9906@gmail.com>
2026-08-02 19:28:39 +0530

    Time: 10 ms (10.85%), Space: 49.7 MB (7.01%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,22 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
+        Integer res[][] = new Integer[piles.length][piles.length];
+        return helper(piles, 0, piles.length - 1, res) > 0;
+    }
+    public int helper(int piles[], int left, int right, Integer res[][]){
+        if (left == right){
+            return piles[left];
+        }
+
+        if (res[left][right] != null){
+            return res[left][right];
+        }
+
+        int pickLeft = piles[left] - helper(piles, left + 1, right, res);
+        int pickRight = piles[right] - helper(piles, left, right - 1, res);
+
+        res[left][right] = Math.max(pickLeft, pickRight);
+
+        return res[left][right];
     }
 }
