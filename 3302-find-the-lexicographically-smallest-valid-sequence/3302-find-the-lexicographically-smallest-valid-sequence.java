df49da058819bd7393766b7e3bb8043979dc66a1
Toshika verma <toshikaverma9906@gmail.com>
2026-08-08 17:38:25 +0530

    Time: 23 ms (100%), Space: 142.3 MB (16.67%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.java
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.java
@@ -1,48 +1,56 @@
 class Solution {
     public int[] validSequence(String word1, String word2) {
-        int dp[] = new int[word1.length() + 1];
-        int ans[] = new int[word2.length()];
+        char[] s = word1.toCharArray();
+        char t[] = word2.toCharArray();
 
-        for (int i=word1.length() - 1; i>=0; i--){
-            dp[i] = dp[i+1];
+        int n = s.length;
+        int m = t.length;
 
-            if (dp[i+1] < word2.length() && word1.charAt(i) == word2.charAt(word2.length() - dp[i+1] - 1)){
-                dp[i]++;
+        int ans[] = new int[m];
+
+        int suffix[] = new int[n+1];
+        int j = m-1;
+        int matched = 0;
+
+        for (int i=n-1; i>=0; i--){
+            if (j >=0 && s[i] == t[j]){
+                matched++;
+                j--;
             }
+            suffix[i] = matched;
         }
 
-        int idx = 0;
-        int start = 0;
-        boolean mismatch = false;
+        int i=0;
+        j=0;
 
-        while(idx < word2.length()){
-            boolean found = false;
+        while (i < n && j < m){
+            if (s[i] == t[j]){
+                ans[j] = i;
+                j++;
 
-            for (int i=start; i<word1.length(); i++){
-                if (word1.charAt(i) == word2.charAt(idx)){
-                    ans[idx] = i;
-                    start = i+1;
-                    idx++;
-                    found = true;
+            }else{
+                if (suffix[i+1] >= m-j-1){
+                    ans[j] = i;
+                    j++;
+                    i++;
                     break;
                 }
-
-                if(!mismatch){
-                    int remaining = word2.length() - idx - 1;
-
-                    if(dp[i+1] >= remaining){
-                        ans[idx] = i;
-                        start= i+1;
-                        idx++;
-                        mismatch = true;
-                        found = true;
-                        break;
-                    }
-                }
             }
-            if (!found ){
-                return new int[0];
+            i++;
+        }
+
+        if (j < m && i == n){
+            return new int[0];
+        }
+        while (i < n && j <m){
+            if (s[i] == t[j]){
+                ans[j] = i;
+                j++;
             }
+            i++;
+        }
+        if (j != m){
+            return new int[0];
         }
         return ans;
     }
