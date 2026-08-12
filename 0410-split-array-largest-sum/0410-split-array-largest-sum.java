4c54c6904f29392728d7c25e79208279ac0add5a
Toshika verma <toshikaverma9906@gmail.com>
2026-08-12 18:56:20 +0530

    Time: 2 ms (29.48%), Space: 43.3 MB (6.99%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -2,16 +2,16 @@ class Solution {
 
     public int splitArray(int[] nums, int k) {
     
-        int left = 0;
+        int left = Integer.MIN_VALUE;
         int right = 0;
 
         for (int num : nums){
             left = Math.max(left,num);
             right += num;
         }
+        int ans = 0;
 
-
-        while (left < right){
+        while (left <= right){
             int mid = left + (right - left) / 2;
             int subarrays = 1;
             int sum = 0;
@@ -27,13 +27,14 @@ class Solution {
             }
 
             if (subarrays <= k){
-                right = mid;
+                ans = mid;
+                right = mid - 1;
 
             }else{
                 left = mid + 1;
             }
         }
 
-        return left;
+        return ans;
     }
 }
