ed69c846715c48fbb982bccc9684c801f22e2910
Toshika verma <toshikaverma9906@gmail.com>
2026-08-23 19:01:06 +0530

    Time: 9 ms (29.93%), Space: 47.3 MB (10.95%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int leftSum = 0;
+        int rightSum = 0;
+
+        int leftQ = 0;
+        int rightQ = 0;
+
+        int mid = num.length() / 2;
+
+        for (int i=0; i<mid; i++){
+            if (num.charAt(i) == '?'){
+                leftQ++;
+
+            }else{
+                leftSum += num.charAt(i) - '0';
+            }
+        }
+
+        for (int i=mid; i<num.length(); i++){
+            if (num.charAt(i) == '?'){
+                rightQ++;
+
+            }else{
+                rightSum += num.charAt(i) - '0';
+            }
+        }
+
+        if ((leftQ + rightQ) % 2 != 0){
+            return true;
+        }
+
+        return 2 * (leftSum - rightSum) != 9 * (rightQ - leftQ);
+
+    }
+}
