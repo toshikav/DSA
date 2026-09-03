7d44b9cf8ef1b9409190a6dc3ef5524409514dca
Toshika verma <toshikaverma9906@gmail.com>
2026-09-03 14:44:07 +0530

    Time: 5 ms (83.33%), Space: 121.6 MB (62.32%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -1,21 +1,18 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
         int min = nums1[0];
+        boolean hasOdd = false;
 
         for (int num : nums1){
             min = Math.min(num, min);
-        }
-
-        if (min % 2 != 0){
-            return true;
-        }
-
-        for (int num : nums1){
             if (num % 2 != 0){
-                return false;
+                hasOdd = true;
             }
         }
 
+        if (min % 2 != 0){
         return true;
+        }
+        return !hasOdd;
     }
 }
